0dae26d73ee2028aba2b8f6086472bf5ebcd24bd
Steve Bargelt <steve@bargelt.com>
2016-08-30 11:11:17 -0700

    Created new_draft_post.sh

---
+++ new_draft_post.sh
@@ -0,0 +1,22 @@
+function new_draft_post(){
+    cd ~/code/stevebargelt.com/_drafts
+    SLUGIFIED="$(echo -n "$1" | sed -e 's/[^[:alnum:]]/-/g' | tr -s '-' | tr A-Z a-z)"
+    SLUG=($SLUGIFIED.md)
+
+    cat > $SLUG <<front_matter
+---
+layout: post
+title: '$1'
+subtitle: 
+portfolio:  
+thumbimage: ''
+image: ''
+author: Steve Bargelt
+category: 
+tags: []
+---
+## Be Brilliant
+front_matter
+  cd ../
+  code .
+}
