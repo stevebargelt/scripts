56f3c0475fc5c8560887d381604704a70d8d7743
Steven.Bargelt <steven.bargelt@target.com>
2017-08-24 15:11:46 -0500

    Function eas spelled wrong in alloff and allon

--- proxy_tgt.sh
+++ proxy_tgt.sh
@@ -1,9 +1,9 @@
-fucntion alloff() {
+function alloff() {
     unset SSL_CERT_FILE
     proxyoff
 }
 
-fucntion allon() {
+function allon() {
     export SSL_CERT_FILE="/Users/z002l7n/.target.crt"
     proxyon
 }
